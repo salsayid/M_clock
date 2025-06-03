75c33db6694ad8a08191c55bebac8f78da798543
salsayid <alsayidoff@gmail.com>
2025-06-03 13:47:03 -0500

    set target date/time

---
+++ script.js
@@ -0,0 +1,8 @@
+//brainstorming logic for the target date
+//it'll be july 1st american central time for me, but july 1st british standard time for her
+//she's ahead by +6 hours sheesh
+
+//1. setting target date/time
+const targetDate = new Date("2025-07-01T00:00:00").getTime();
+
+//ima just add my time bruh
